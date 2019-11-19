b8603fd3bcf4d2402d1584671255093343016e8b
zs <zs@bjpowernode.com>
2019-11-19 20:55:46 +0800

    添加属性

--- src/main/java/com/bjpowernode/domain/User.java
+++ src/main/java/com/bjpowernode/domain/User.java
@@ -12,6 +12,23 @@ public class User {
     private int id;
     private String name;
     private String address;
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
     public String getName() {
         return name;
