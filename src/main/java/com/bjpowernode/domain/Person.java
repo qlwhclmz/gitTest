e825d4b168c88a2242cad016574efb6facc1124d
zs <zs@bjpowernode.com>
2019-11-21 21:35:44 +0800

    添加属性

---
+++ src/main/java/com/bjpowernode/domain/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.domain
+ * Description
+ *
+ * @date:2019/11/21 21:34
+ * @author:lsz
+ */
+public class Person {
+    private int id;
+}
